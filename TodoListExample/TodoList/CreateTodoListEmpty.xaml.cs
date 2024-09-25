d94ffd3ef10ea97f452c2db13a4c4c0341f6900f
Adrian Czubaty <adriancz.prywatny@gmail.com>
2024-09-25 20:58:32 +0200

    Added hint to AddTodoView.xaml in the input section

---
+++ TodoListExample/TodoList/CreateTodoListEmpty.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TodoListExample.TodoList;
+
+public partial class CreateTodoListEmpty {
+    public CreateTodoListEmpty() {
+        InitializeComponent();
+
+        DataContext = this;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+        if (DataContext is not CreateTodoListEmptyViewModel viewModel) return;
+        
+        Visibility = Visibility.Hidden;
+        // viewModel
+    }
+}
---
+++ TodoListExample/TodoList/CreateTodoListEmptyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using TodoListExample.Enums;
+
+namespace TodoListExample.TodoList;
+
+public partial class CreateTodoListEmptyViewModel : ObservableObject {
+    private IMessenger Messenger { get; }
+
+    public CreateTodoListEmptyViewModel(IMessenger messenger) {
+        Messenger = messenger;
+    }
+
+    [RelayCommand]
+    public void OnCreateFirstTodo() {
+        var todoList = new Data.TodoList {
+            Title = "My first todo list"
+        };
+
+        Messenger.Send(new TodoListUpdates(todoList, UpdateTypeEnum.Create));
+    }
+}
